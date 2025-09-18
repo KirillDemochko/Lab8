4c476d97ea7517337975e13f656609f0df71e5a9
KirillDemochko <kirilldemocko17825@gmail.com>
2025-09-18 21:03:30 +0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package org.example.commands;
-
-import org.example.data.User;
-
-public interface Command {
-    String execute(String[] args, User user) throws Exception;
-    String getDescription();
-}
